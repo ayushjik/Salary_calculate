c8e6c065d2c6825cb60e62c44169ab7ec7f59c22
ayushjik <112191793+ayushjik@users.noreply.github.com>
2025-05-16 19:16:32 +0530

    Update microservices.ts

--- auth/microservices.ts
+++ auth/microservices.ts
@@ -1,8 +1,5 @@
-import { PrismaClient } from '@prisma/client';
-import { error } from 'console';
 import * as jwt from 'jsonwebtoken';
 
-const prisma = new PrismaClient();
 
 const SECRET = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
 
